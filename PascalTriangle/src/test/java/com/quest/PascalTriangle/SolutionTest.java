022a8eea81df2cb1074fe881ad6d6e6286e7429e
atusharm7 <atusharm7@WKWIN9158344.global.publicisgroupe.net>
2021-01-26 12:21:22 +0530

    Pascal triangle solution have been added.

--- PascalTriangle/src/test/java/com/quest/PascalTriangle/SolutionTest.java
+++ PascalTriangle/src/test/java/com/quest/PascalTriangle/SolutionTest.java
@@ -2,7 +2,11 @@ package com.quest.PascalTriangle;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
 import org.junit.Test;
+
+import com.quest.PascalTriangle.solution.Solution;
 
 /**
  * Unit test for simple App.
@@ -15,6 +19,21 @@ public class SolutionTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        Solution solution = new Solution();
+        List<List<Integer>> result = solution.generate(2);
+        System.out.println(result);
+    	assertTrue( true );
+    }
+    
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue1()
+    {
+        Solution solution = new Solution();
+        List<List<Integer>> result = solution.generate(5);
+        System.out.println(result);
+    	assertTrue( true );
     }
 }
