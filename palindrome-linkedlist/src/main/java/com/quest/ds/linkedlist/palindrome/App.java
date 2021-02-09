1147ef01bc8c469e5ba9937f3fef712e7defeae5
atusharm7 <atusharm7@WKWIN9158344.global.publicisgroupe.net>
2021-02-09 20:07:39 +0530

    Implemented solution of palidrome-linkedlist.

---
+++ palindrome-linkedlist/src/main/java/com/quest/ds/linkedlist/palindrome/App.java
@@ -0,0 +1,13 @@
+package com.quest.ds.linkedlist.palindrome;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ palindrome-linkedlist/src/main/java/com/quest/ds/linkedlist/palindrome/Solution.java
@@ -0,0 +1,38 @@
+package com.quest.ds.linkedlist.palindrome;
+
+public class Solution {
+
+	public boolean isPalindrome(ListNode head) {
+
+		ListNode slow = head;
+		ListNode fast = head;
+
+		while (fast != null && fast.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+
+		slow = reversed(slow);
+		fast = head;
+
+		while (slow != null) {
+			if (slow.val != fast.val) {
+				return false;
+			}
+			slow = slow.next;
+			fast = fast.next;
+		}
+		
+		return true;
+	}
+
+	private ListNode reversed(ListNode head) {
+		ListNode prev = null;
+		while(head != null) {
+			ListNode next = head.next;
+			prev = head;
+			head = next;
+		}
+		return prev;
+	}
+}
